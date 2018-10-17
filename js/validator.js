4c466b82c57bf3f3fc3418b65e292ff33feef0e6
Kushagra Garg <you@example.com>
2018-10-17 22:40:16 +0530

    form updated

--- js/validator.js
+++ js/validator.js
@@ -20,26 +20,64 @@ function match(){
 						}
 				}
 function  Photography_1(){
-	document.getElementById("wedding").selected="true";
-
+	document.getElementById("wedding").selected=true;
+	console.log();
+	document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 function  Photography_2(){
-	document.getElementById("engagement").selected="true";
+	document.getElementById("engagement").selected=true;
+	document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 
 }
 function  Photography_3(){
-document.getElementById("maternity").selected="true";
+document.getElementById("maternity").selected=true;
+document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 
 }
 function Photography_4(){
-document.getElementById("kids").selected="true";
+document.getElementById("kids").selected=true;
+document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 
 }
 function  Photography_5(){
-document.getElementById("birthday").selected="true";
+document.getElementById("birthday").selected=true;
+document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 
 }
 function  Photography_6(){
-document.getElementById("pre-wedding").selected="true";
+document.getElementById("pre-wedding").selected=true;
+document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 
+}
+function single(){
+	document.getElementById("datepicker").disabled = false;
+	document.getElementById("datepicker").placeholder = "Date";
+	document.getElementById("datepicker").required = true;
+	document.getElementById("datetimepicker3").disabled = true;
+	document.getElementById("datepicker2").disabled = true;
+	document.getElementById("datetimepicker").disabled = true;
+}
+function multi(){
+	document.getElementById("datepicker").disabled = false;
+	document.getElementById("datepicker").required = true;
+	document.getElementById("datetimepicker3").disabled = false;
+	document.getElementById("datetimepicker3").required = true;
+	document.getElementById("datepicker2").disabled = false;
+	document.getElementById("datepicker2").required = true;
+	document.getElementById("datetimepicker").disabled = false;
+	document.getElementById("datetimepicker").required = true;
+}
+function hourly(){
+	document.getElementById("datepicker").disabled = true;
+	document.getElementById("datetimepicker3").disabled = false;
+	document.getElementById("datetimepicker3").required = true;
+	document.getElementById("datepicker2").disabled = true;
+	document.getElementById("datetimepicker").disabled = false;
+	document.getElementById("datetimepicker").required = true;
 }
